4c366b7b89c8f806a7a44b7a2f0ed589c14a86ed
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-11-06 21:38:43 +0200

    Problem 4

--- problems2_6.m
+++ problems2_6.m
@@ -1,4 +1,4 @@
-%% in sample (training set) without regularization
+%% in sample (training set) without regularization (problem 2)
 
 train = load('train.txt');
 X = train(:, 1:2); y = train(:, 3);
@@ -15,7 +15,7 @@ yEst = sign(X * w);
 
 % determine Ein (prediction accuracy)
 Ein = length(yEst(yEst~=y))/ length(y);
-fprintf('Ein: %f\n', Ein);
+fprintf('Ein without regularization: %f\n', Ein);
 
 %% out of sample (test set) without regularization
 test = load('test.txt');
@@ -29,26 +29,31 @@ predictions = sign(Xtest * w);
 
 % determine Eout
 Eout = length(predictions(predictions~=ytest))/ length(ytest);
-fprintf('Eout: %f\n', Eout);
+fprintf('Eout without regularization: %f\n\n', Eout);
 
-%% with regularization
-lambda = 10^-3; k = size(X, 2);
+%% with regularization (problem 3 and 4)
+lambda = [10^-3 10^3]; k = size(X, 2);
 I = eye(k); I(1, 1) = 0;
 
-% use normal equations to find the weights
-w_reg = pinv(X' * X + lambda * I) * X' * y;
+for i = 1:length(lambda)
+    % use normal equations to find the weights
+    w_reg = pinv(X' * X + lambda(i) * I) * X' * y;
 
-% extimate y according to w_reg
-yEst_reg = sign(X * w_reg);
+    % extimate y according to w_reg
+    yEst_reg = sign(X * w_reg);
 
-% determine Ein with regularization
-Ein_reg = length(yEst_reg(yEst_reg~=y))/ length(y);
-fprintf('Ein with regularization: %f\n', Ein_reg);
+    % determine Ein with regularization
+    Ein_reg = length(yEst_reg(yEst_reg~=y))/ length(y);
+    fprintf('Ein with regularization and lambda=%f: %f\n', lambda(i), ...
+        Ein_reg);
 
-predictions_reg = sign(Xtest * w_reg);
+    predictions_reg = sign(Xtest * w_reg);
 
-% determine Eout with regularization
-Eout_reg = length(predictions_reg(predictions_reg~=ytest))/ length(ytest);
-fprintf('Eout with regularization: %f\n', Eout_reg);
+    % determine Eout with regularization
+    Eout_reg = length(predictions_reg(predictions_reg~=ytest))/ ...
+        length(ytest);
+    fprintf('Eout with regularization and lambda=%f: %f\n\n', lambda(i), ...
+        Eout_reg);
+end
 
 %%
