641e88888dd10cfb44e7469bd8a857d8e00b85b5
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-11-06 21:27:00 +0200

    Problem 3

--- problems2_6.m
+++ problems2_6.m
@@ -1,4 +1,4 @@
-%% in sample (training set)
+%% in sample (training set) without regularization
 
 train = load('train.txt');
 X = train(:, 1:2); y = train(:, 3);
@@ -17,7 +17,7 @@ yEst = sign(X * w);
 Ein = length(yEst(yEst~=y))/ length(y);
 fprintf('Ein: %f\n', Ein);
 
-%% out of sample (test set)
+%% out of sample (test set) without regularization
 test = load('test.txt');
 Xtest = test(:, 1:2); ytest = test(:, 3);
 Xtest = transform(Xtest); n = size(Xtest, 1);
@@ -31,3 +31,24 @@ predictions = sign(Xtest * w);
 Eout = length(predictions(predictions~=ytest))/ length(ytest);
 fprintf('Eout: %f\n', Eout);
 
+%% with regularization
+lambda = 10^-3; k = size(X, 2);
+I = eye(k); I(1, 1) = 0;
+
+% use normal equations to find the weights
+w_reg = pinv(X' * X + lambda * I) * X' * y;
+
+% extimate y according to w_reg
+yEst_reg = sign(X * w_reg);
+
+% determine Ein with regularization
+Ein_reg = length(yEst_reg(yEst_reg~=y))/ length(y);
+fprintf('Ein with regularization: %f\n', Ein_reg);
+
+predictions_reg = sign(Xtest * w_reg);
+
+% determine Eout with regularization
+Eout_reg = length(predictions_reg(predictions_reg~=ytest))/ length(ytest);
+fprintf('Eout with regularization: %f\n', Eout_reg);
+
+%%
